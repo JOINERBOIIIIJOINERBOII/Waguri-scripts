01d5a81bf0757baacb0b064e8776688848e51e71
JOINERBOIIIIJOINERBOII <potangpogivlog@gmail.com>
2026-05-22 19:22:38 +0800

    Update music.js

--- api/music.js
+++ api/music.js
@@ -1,1 +1,14 @@
 
+// api/music.js - Host your own music file
+// You can also just use a direct URL to any MP3
+
+export default async function handler(req, res) {
+    // Option 1: Redirect to a music URL
+    // res.redirect('https://example.com/music.mp3');
+    
+    // Option 2: Serve your own music (upload an MP3 to /public/music.mp3)
+    // res.sendFile('music.mp3', { root: './public' });
+    
+    // For now, redirect to a free anime music
+    res.redirect('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+}
